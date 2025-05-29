1b39b1fa4da3b2a4ecabfe1b9e4f2142c9aa41ac
Aleksa <aleksamlsvc12@gmail.com>
2025-05-29 14:58:02 +0200

    Delete Job functional + Toast notification

--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,16 +1,37 @@
 <script setup>
   import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
   import {reactive, onMounted} from 'vue';
-  import {useRoute, RouterLink} from 'vue-router';
+  import {useRoute, RouterLink, useRouter} from 'vue-router';
   import axios from 'axios';
   import BackButton from '@/components/BackButton.vue';
+  import {useToast} from 'vue-toastification';
 
   const route = useRoute();
+
+  const router = useRouter();
+  const toast = useToast();
+
   const jobId = route.params.id;
   const state = reactive({
     job: {},
     isLoading: true,
   });
+
+  const deleteJob = async () => {
+    try {
+      const confirm = window.confirm('Are you sure?');
+      if(confirm){
+        await axios.delete(`/api/jobs/${jobId}`);
+        toast.success('Job Deleted Successfully');
+        router.push('/jobs');
+      }
+ 
+    } catch (error) {
+      console.error('Error deleting job', error);
+      toast.error('Job not Deleted');
+    }
+  };
+
 
   onMounted(async () => {
     try {
@@ -93,7 +114,7 @@
                 class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job</RouterLink
               >
-              <button
+              <button @click="deleteJob()"
                 class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
                 Delete Job
